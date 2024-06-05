70d09b3eb63264175885ed773b3c1592528cf437
evcruz3 <erickson49366@gmail.com>
2024-06-05 12:06:57 +0800

    updated date fakers

--- fakers/case_report_generator.py
+++ fakers/case_report_generator.py
@@ -19,6 +19,16 @@ def generate_case_report(gdf, faker):
     age_group_19_to_58_cases = random.randint(0, number_of_cases - (age_group_0_to_4_cases + age_group_5_to_18_cases))
     age_group_60_plus_cases = random.randint(0, (number_of_cases - (age_group_0_to_4_cases + age_group_5_to_18_cases + age_group_19_to_58_cases)))
     age_group_unknown_cases = number_of_cases - (age_group_0_to_4_cases + age_group_5_to_18_cases + age_group_19_to_58_cases + age_group_60_plus_cases)
+
+    from_date = faker.date_time_between(start_date="-365d", end_date="now")
+    from_date_string = from_date.isoformat()
+
+    # to_date must be after from_date
+    to_date = faker.date_time_between(start_date=from_date, end_date="-7d")
+    to_date_string = to_date.isoformat()
+
+    reporting_date = faker.date_time_between(start_date=to_date, end_date="now")
+    reporting_date_string = reporting_date.isoformat()
     
     case_report = {
         "caseType": random.choice(case_types),
@@ -27,9 +37,10 @@ def generate_case_report(gdf, faker):
             "latitude": coordinate.y,
             "longitude": coordinate.x
         },
-        "from": faker.date_time_between(start_date="-365d", end_date="-335d").isoformat(),
-        "to": faker.date_time_between(start_date="-334d", end_date="now").isoformat(),
-        "reportingDate": faker.date_time_between(start_date="-334d", end_date="now").isoformat(),
+        "from": from_date_string,
+        # to must be after from
+        "to": to_date_string,
+        "reportingDate": reporting_date_string,
         "reportingEntityType": random.choice(reporting_entity_types),
         "sexGroupMaleCases": male_cases,
         "sexGroupFemaleCases": female_cases,
